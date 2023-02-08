d5d7fd37c39d43fd9d9184529a563741d607319b
C-Skala <cjskala@gmail.com>
2023-02-07 22:17:12 -0500

    finally got it to work

--- Selenium_Scrape.py
+++ Selenium_Scrape.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 import csv
 
 #set up CSV
@@ -16,24 +19,38 @@ options.add_experimental_option("detach", True)
 scraper = webdriver.Chrome(service = browser_driver, options = options)
 
 #Get the page
-scraper.get('https://webscraper.io/test-sites/e-commerce/static/computers/laptops')
-
+scraper.get('https://webscraper.io/test-sites/e-commerce/static/computers/laptops?page=1')
+        
+wait = WebDriverWait(scraper, 10)
+popup = scraper.find_element(By.CLASS_NAME, 'acceptCookies')
+wait.until(EC.visibility_of(popup))
+popup.click()
 
 #grab all laptops by class comp
 unique_id = 1
+x=2
+page_runs = True
 while True:
     comps = scraper.find_elements(By.CLASS_NAME, "thumbnail")
     for comp in comps:
-        comp_name = comp.find_element(By. CLASS_NAME, 'title')
+        comp_name = comp.find_element(By.CLASS_NAME, 'title').get_attribute('title')
         price = comp.find_element(By. CLASS_NAME, 'caption')
         specs = comp.find_element(By. CLASS_NAME, 'description')
         reviews = comp.find_element(By.CLASS_NAME, 'ratings')
         writer.writerow(
-            [unique_id, comp_name.text, price.text, specs.text, reviews.text[:2]])
+            [unique_id, specs.text, price.text, specs.text, reviews.text[:2]])
         unique_id += 1
         
     try:
-        element = scraper.find_element(By.XPATH, '/html/body/div[1]/div[3]/div/div[2]/ul/li[13]/a')
+        
+        #while page_runs ==True:
+        #    if x != 20:
+        #        scraper.get(f'https://webscraper.io/test-sites/e-commerce/static/computers/laptops?page={x}')
+        #        x+=1
+        #    else:
+        #        page_runs = False
+            
+        element = scraper.find_element(By.PARTIAL_LINK_TEXT, '›')
         element.click()
     except:
         break
